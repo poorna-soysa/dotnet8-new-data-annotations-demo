8422180501516ea8f6c68cf132447043049c575c
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-04-30 06:23:19 +0530

    Add project files.

---
+++ NewDataAnnotationsDemoInDotNet8/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewDataAnnotationsDemoInDotNet8.Models;
+
+namespace NewDataAnnotationsDemoInDotNet8.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateProduct(Product product)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        return Ok(product);
+    }
+}
---
+++ NewDataAnnotationsDemoInDotNet8/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewDataAnnotationsDemoInDotNet8.Models;
+
+public class Product
+{
+    [Length(2, 30)]
+    public string Name { get; set; }
+
+    [Length(2, 255)]
+    public string Description { get; set; }
+
+    [Range(1, 1000, MinimumIsExclusive = true, MaximumIsExclusive = false)]
+    public decimal Price { get; set; }
+
+    [AllowedValues("S", "M", "L", "XL", "XXL")]
+    public string Size { get; set; }
+
+    [DeniedValues("Electronic", "Computers")]
+    public string Category { get; set; }
+
+    [Base64String]
+    public string Image { get; set; }
+}
